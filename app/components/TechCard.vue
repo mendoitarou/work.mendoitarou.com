7f6f3004fbd477cf51defaf16eddf7eb6db196e6
mendoitarou <42207304+mendoitarou@users.noreply.github.com>
2026-03-15 22:17:50 +0900

    Add_Component

---
+++ app/components/TechCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  cards: {
+    type: Object
+  }
+})
+</script>
+
+<template>
+  <div>
+    <UPageCard
+        :title="cards.title"
+    >
+        <UPageColumns>
+            <UPageCard
+                v-for="(middleCard, middleIndex) in cards.items"
+                :key="middleIndex"
+                :title="middleCard.title"
+            >
+                <UPageCard
+                    v-for="(card, index) in middleCard.items"
+                    :key="index"
+                    :icon="card.icon"
+                    :title="card.title"
+                    :description="card.description"
+                >
+                </UPageCard>
+            </UPageCard>
+        </UPageColumns>
+    </UPageCard>
+  </div>
+</template>
