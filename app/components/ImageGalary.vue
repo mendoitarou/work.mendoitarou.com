ea85ad9ec62118ca0507f4cdf31b582115e03f3b
mendoitarou <42207304+mendoitarou@users.noreply.github.com>
2026-03-20 00:20:17 +0900

    Change_ImageDesign

--- app/components/ImageGalary.vue
+++ app/components/ImageGalary.vue
@@ -11,7 +11,13 @@ defineProps({
         <br><USeparator /><br>
         <p>Galary</p>
         <UCarousel v-slot="{ item }" arrows :prev="{ color: 'primary' }" :next="{ variant: 'solid' }" :items="images" class="w-full mx-auto">
-            <img :src="item" width="1280" height="720" class="rounded-lg" loading="lazy">
+            <UModal>
+                <img :src="item" width="1280" height="720" class="rounded-lg h-full max-h-[720px]" loading="lazy">
+
+                <template #content>
+                    <img :src="item" width="1280" height="720" class="rounded-lg" loading="lazy">
+                </template>
+            </UModal>
         </UCarousel>
     </div>
 </template>
