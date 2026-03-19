e27948f1931022cc2fab92d60f78a06deac3265d
mendoitarou <42207304+mendoitarou@users.noreply.github.com>
2026-03-19 21:20:44 +0900

    Change_WorkList-Design

--- app/components/SectionWorkPin.vue
+++ app/components/SectionWorkPin.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 const { data: myWorkPin } = await useAsyncData('myWork-list-pin', () => {
-  return queryCollection('works')
-    .where('draft', '=', false)
-    .where('pinned', '=', true)
-    .order('id', 'DESC')
-    .all()
+    return queryCollection('works')
+        .where('draft', '=', false)
+        .where('pinned', '=', true)
+        .order('id', 'DESC')
+        .all()
 })
 </script>
 
@@ -14,16 +14,9 @@ const { data: myWorkPin } = await useAsyncData('myWork-list-pin', () => {
             <UPageHeader headline="厳選" />
             <br>
             <UPageGrid>
-                <UPageCard
-                    v-for="(myWork, index) in myWorkPin"
-                    :key="index"
-                    highlight
-                    highlight-color="primary"
-                    :icon="myWork.icon"
-                    :title="myWork.title"
-                    :description="myWork.description"
-                    :to="myWork.path"
-                />
+                <UBlogPost v-for="(myWork, index) in myWorkPin" :key="index" :icon="myWork.icon" :title="myWork.title"
+                    :description="myWork.description" :to="myWork.path" :date="myWork.createDate"
+                    :badge="{ icon: myWork.icon, label: myWork.mainCategory, color: 'secondary', variant: 'solid' }" />
             </UPageGrid>
         </UContainer>
     </div>
--- app/pages/works/index.vue
+++ app/pages/works/index.vue
@@ -18,8 +18,21 @@ const { data: myWorks } = await useAsyncData('myWork-list', () => {
       <USeparator />
       <br>
       <UPageGrid>
-        <UPageCard v-for="(myWork, index) in myWorks" :key="index" :icon="myWork.icon" :title="myWork.title"
-          :description="myWork.description" :to="myWork.path" />
+        <UBlogPost
+          v-for="(myWork, index) in myWorks"
+          :key="index"
+          :icon="myWork.icon"
+          :title="myWork.title"
+          :description="myWork.description"
+          :to="myWork.path"
+          :date="myWork.createDate"
+          :badge="{
+            icon: myWork.icon,
+            label: myWork.mainCategory,
+            color: 'secondary',
+            variant: 'solid'
+          }"
+        />
       </UPageGrid>
     </UContainer>
   </div>
