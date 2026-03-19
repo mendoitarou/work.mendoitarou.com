810c7549be036f81f4aa6d476d63613f34cf5efb
mendoitarou <42207304+mendoitarou@users.noreply.github.com>
2026-03-19 23:05:30 +0900

    Fix_Image-Size

--- app/components/ImageGalary.vue
+++ app/components/ImageGalary.vue
@@ -10,7 +10,7 @@ defineProps({
     <div>
         <br><USeparator /><br>
         <p>Galary</p>
-        <UCarousel v-slot="{ item }" arrows :prev="{ color: 'primary' }" :next="{ variant: 'solid' }" :items="images" class="w-full max-w-xs mx-auto">
+        <UCarousel v-slot="{ item }" arrows :prev="{ color: 'primary' }" :next="{ variant: 'solid' }" :items="images" class="w-full mx-auto">
             <img :src="item" width="1280" height="720" class="rounded-lg" loading="lazy">
         </UCarousel>
     </div>
